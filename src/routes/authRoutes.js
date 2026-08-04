b5ec80596350cb2b56ece3e43c50a97da2ca6a16
freeeeirannnn-eng <freeeeirannnn@gmail.com>
2026-08-04 23:51:17 +0330

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const AuthController = require('../controllers/authController');
-const { verifyToken } = require('../middleware/authMiddleware');
+const authController = require('../controllers/authController');
 
-router.post('/login', AuthController.login);
-router.post('/change-password', verifyToken, AuthController.changePassword);
+router.post('/login', authController.login);
 
 module.exports = router;
